ca9a7729c952c55d50ea10fb80329586ec6b0d25
QCAILLE <48440775+QCAILLE@users.noreply.github.com>
2020-02-07 13:34:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,15 +9,20 @@
 	<title>Supervision - MES BEMA</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<h1 align="center">Interface de supervision - machine BEMA - PROMEO Compiègne</h1>
 
-	<?php
-		include('compte/connexion.php');
-	?>
-
-	<a href="historique.php">Consulter l'Historique utilisateur</a>
+		<ul>
+			<li><a href="historique.php"><i class="fa fa-fw fa-search"></i> Consulter l'Historique utilisateur</a> </li>
+			<li><a href="compte/inscription.php"> Ajouter un nouvel utilisateur</a></li>
+			<li><a href="stock.php"> Voir les stocks matériels</a></li>
+			<li class="connect"><a href="compte/connexion.php"><i class="fa fa-fw fa-user"></i> Se connecter</a></li>
+			<li class="connect"><a href="compte/editionprofil.php"> Editer mon profil</a></li>
+		</ul>
+		
+			<br />
 
 </body>
 </html>
