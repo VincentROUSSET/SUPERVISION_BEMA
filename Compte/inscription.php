aa87f0d5be730bb8198f537c18febbe8e3b6d47d
QCAILLE <48440775+QCAILLE@users.noreply.github.com>
2020-02-07 14:35:39 +0100

    Update inscription.php

--- Compte/inscription.php
+++ Compte/inscription.php
@@ -6,7 +6,7 @@
 	$donneesDroitAcces = $reqDroitAcces->fetchAll();
 	$donneesGroupe = $reqGroupe->fetchAll();
 	var_dump($donneesDroitAcces);
-	var_dump($donneesGroupe)
+	var_dump($donneesGroupe);
 	
 	if (isset($_POST['forminscription'])) 
 {
@@ -14,9 +14,12 @@
 	$prenom = htmlspecialchars($_POST['prenom']);
 	$email = htmlspecialchars($_POST['email']);
 	$email2 = htmlspecialchars($_POST['email2']);
+	$identifiant = htmlspecialchars($_POST['identifiant']);
 	$mdp = sha1($_POST['mdp']);
 	$mdp2 = sha1($_POST['mdp2']);
-	if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['email']) AND !empty($_POST['email2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2']))
+	$id_droits_acces = htmlspecialchars($_POST['id_droits_acces']);
+	$id_groupe = htmlspecialchars($_POST['id_groupe']);
+	if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['email']) AND !empty($_POST['email2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2']) AND !empty($_POST['id_droits_acces']) AND !empty($_POST['id_groupe']))
 	{
 		
 		$nom_length = strlen($nom);
@@ -32,8 +35,10 @@
 					{
 						if($mdp == $mdp2)
 						{
-							$insertuser = $bdd->prepare("INSERT INTO utilisateur(nom, prenom, email) VALUES (?, ?, ?)");
-							$insertuser->execute(array($nom, $prenom, $email));
+							$insertuser = $bdd->prepare("INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `id_droits_acces`, `id_groupe`) VALUES (NULL, '?', '?', '?', '?', '?')");
+							$insertuser->execute(array($nom, $prenom, $email, $id_droits_acces, $id_groupe));
+							$insertaccount = $bdd->prepare("INSERT INTO `compte` (`id`, `identifiant`, `password`, `id_utilisateurs`) VALUES (NULL, 'test', 'test', '1') ");
+							$insertuser->execute(array($identifiant, $mdp, $nom));
 							$erreur = "Votre compte à bien été créer ! <a href=\"connexion.php\">Me connecter</a>";	
 						}
 						else
@@ -80,9 +85,9 @@
 			<br /> <br />
 			<form method="POST" action="">
 
-				<label for="Nom">Nom : </label>
+				<label for="nom">Nom : </label>
 
-				<input type="text" placeholder="" id="Nom" name="Nom">
+				<input type="text" placeholder="" id="nom" name="nom">
 
 				<label for="prenom">Prénom : </label>
 
@@ -90,7 +95,7 @@
 
 				<label for="Identifiant">Identifiant :</label>
 
-				<input type="text" placeholder="" id="dentifiant" name="Identifiant">
+				<input type="text" placeholder="" id="identifiant" name="identifiant">
 
 				<label for="mdp">Mot de passe :</label>
 
@@ -109,21 +114,17 @@
 				<input type="email" placeholder="" id="email2" name="email2">
 
 				<label>Sélectionnez l'Habilitation : </label>
-				        <select name="">
+				        <select name="id_droits_acces">
 				        	<?php
 				        	
 				        	  foreach($donneesDroitAcces as $valueDroitAcces){ ?>
 
 				        
-						        <option><?php echo $valueDroitAcces['type'] ?></option>";
+						        <option value="id_droits_acces"><?php echo $valueDroitAcces['type'] ?></option>";
 
 						    <?php
-
-				   			  }
-				   			 
+				   			  } 
 				   		    ?>
-						      
-
 						   
 				        </select>
 				        
@@ -131,13 +132,13 @@
 						<br />
 
 				<label>Sélectionnez le groupe : </label>
-				        <select name="">
+				        <select name="id_groupe">
 				        	<?php
 				        	
 				        	  foreach($donneesGroupe as $valueGroupe){ ?>
 
 				        
-						        <option><?php echo $valueGroupe['nom_groupe'] ?></option>";
+						        <option value="id_groupe"><?php echo $valueGroupe['nom_groupe'] ?></option>";
 
 						    <?php
 
@@ -155,6 +156,9 @@
 				<input type="submit" name="forminscription" value="Créer un nouvel utilisateur">
 				<a href="../index.php">Retour</a>
 			</form>
+			<?php
+			echo "$erreur";
+			?>
 	
 </body>
 </html>
