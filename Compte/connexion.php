ecd6a1244fab314ebd51ac225e3d95fc7bf9e200
QCAILLE <48440775+QCAILLE@users.noreply.github.com>
2020-02-07 13:35:56 +0100

    Update connexion.php

--- Compte/connexion.php
+++ Compte/connexion.php
@@ -1,45 +1,32 @@
 <?php
-
-	include('BDD.php');
-
-	
-
+	include('../BDD.php');
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
 	<link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
 </head>
 <body>
+	<div class="container">
+		<h1 align="center">Connexion</h1>
 	<form method="POST" action="">
-		<table align="center">
-			<tr>
-				<td align="right">
+
 					<label class="label_id" for="pseudo">Entrez votre identifiant</label>
-				</td>
-				<td>
-					<input type="text" placeholder="Votre Pseudo" id="ident" name="pseudo">
-				</td>
-			</tr>
-			<tr>
-				<td align="right">
+
+					<input type="text" placeholder="" id="identifiant" name="identifiant">
+
 					<label class="label_mdp" for="mdp">Entrez votre mot de passe</label>
-				</td>
-				<td>
-					<input type="password" placeholder="Votre mot de passe" id="mdp" name="mail">
-				</td>
-			</tr>
-			<tr>
-				<td align="right">
+
+					<input type="password" placeholder="" id="mdp" name="mdp">
+
 					<input type="submit" name="" value="Se connecter">
-				</td>
-			</tr>
-		</table>
+
+					<a href="../index.php" id="retour">Retour</a>
+
 	</form>
-	
+	</div>
 </body>
 </html>
